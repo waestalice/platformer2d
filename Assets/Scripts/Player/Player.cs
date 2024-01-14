9d0ec68bc6f360af3fbe56ad2f082a6f328ecd8b
waestalice <alice.barreto890@gmail.com>
2024-01-14 01:57:29 -0300

    add jump to prototype

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,11 +6,21 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
+    public Vector2 friction = new Vector2(.1f, 0);
+
     public Vector2 velocity;
 
     public float speed;
 
+    public float forceJump = 2;
+
     private void Update()
+    {
+        HandleJump();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
@@ -22,5 +32,20 @@ public class Player : MonoBehaviour
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         }
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+    }
+    
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            myRigidbody.velocity = Vector2.up * forceJump;
     }
 }
